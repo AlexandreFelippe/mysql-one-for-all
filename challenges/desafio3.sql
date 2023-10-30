31c2398610a42912faa07fcd7aa2cd1c9c1ea8ef
AlexandreFelippe <alemfelippe@gmail.com>
2023-10-30 12:21:19 -0300

    Resolve exercicio 03

---
+++ challenges/desafio3.sql
@@ -0,0 +1,12 @@
+SELECT 
+    pu.nome AS 'pessoa_usuaria',
+    COUNT(hr.musica_id) AS 'musicas_ouvidas',
+    SUM(m.duracao / 60) AS 'total_minutos'
+FROM
+    pessoas_usuarias pu
+        INNER JOIN
+    hist_de_reproducao hr ON pu.pessoa_usuaria_id = hr.pessoa_usuaria_id
+        INNER JOIN
+    musicas m ON m.musica_id = hr.musica_id
+GROUP BY pu.nome
+ORDER BY pessoa_usuaria;
